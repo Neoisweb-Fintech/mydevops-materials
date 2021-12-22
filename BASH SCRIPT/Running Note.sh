727c964a6f5fd98a76876099ab96b2f87c0a5213
gbenga <israekg@yahoo.com>
2021-12-22 08:45:01 -0800

    mydevops notes

---
+++ BASH SCRIPT/Running Note.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo "Devops is great"
+echo "studying Devopsnow with your family"
+whoami
+pwd
+echo "today's date is date"
+
+echo 'what is your name"
+read name
+ 
+echo "Enter your pin"
+read pin
+
+echo 'Enter the name of the New-Hire"
+echo "creating an account of the new - hire"
+sudo adduser $Name
+passwd $name
+echo "account created and password assigned to $Name"
+
+
+vi vars3.sh
+#!/bin/bash
+echo "Welcome to the World of Entertainment"
+echo "Please enter your name"
+read name
+echo "The name you entered is #name"
+echo "is this name given correct"
+read answer
+echo "Who you like to transfer today"
+read answer
+echo "enter your pin number"
+read answer
+echo "You have sucessfully login -in"
+echo "Thanks for your sincere patronge"
+
+
+#!/bin/bash
+echo "WELCOME TO THE BANK OF AMERICA
+echo "Please Slot in your card today"
+echo "Please enter the PIN number"
+read pin
+echo "The pin entered is $PIN"
+echo "is that correct?."
+read answer
+echo "You have successfully login in"
+echo "Enjoy doing Business with US today"
+echo "Thanks"
+
+
+
+
+To create a EATERY services script
+
+echo "WELCOME TO MCDONALD"
+echo "Start ordering"
+echo "Here are the list of our MEAL menu for today"
+echo "Sandwitche burger"
+
+db.sql
+userNames;
+Johnas in gi
